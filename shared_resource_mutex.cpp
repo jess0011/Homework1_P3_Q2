a29181c7164157015936cbd1c94acf96c31930de
jess0011 <106406684+jess0011@users.noreply.github.com>
2022-10-09 18:05:28 -0700

    Update shared_resource_mutex.cpp

--- shared_resource_mutex.cpp
+++ shared_resource_mutex.cpp
@@ -4,7 +4,7 @@
 #define iterations 300000000
 long long shared_resource = 0;
 
-pthread_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutext = PTHREAD_MUTEX_INITIALIZER;
 //Thread function to modify shared resource
 void* inc_dec_resource(void* arg){
 
@@ -23,11 +23,16 @@ int main(void){
     pthread_t tid1, tid2;
     int value1 = 1;
 
-    pthread_create(&tid1, NULL, inc_dec_resource, &value2);
-    pthread_join(tid1, NULL);
-
+    pthread_create(&tid1, NULL, inc_dec_resource, &value1);
+    
+    //Thread 2 to increment shared resource
+    int value2 = -1;
+    pthread_create(&tid2, NULL, inc_dec_resource, &value2);
+    pthread_join(tib1, NULL);
     pthread_join(tid2, NULL);
+    
     printf("Shared resource value: %lld\n", shared_resource);
-
+    
     return 0;
+}
 }
